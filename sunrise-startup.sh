8310661e094a0c7f0abc2c39109fb5363af2ea46
SimonBrazell <simon@brazell.com.au>
2021-05-23 18:59:08 +1000

    fix: Fetch sunrise for the correct day.

--- sunrise-startup.sh
+++ sunrise-startup.sh
@@ -5,11 +5,10 @@ LONGITUDE="YOUR_LONGITUDE"
 MAC_ADDRESS="YOUR_MAC_ADDRESS"
 BROADCAST_ADDRESS="YOUR_BROADCAST_ADDRESS"
 
-SUNRISE=$(curl -s "https://api.sunrise-sunset.org/json?lat=$LATITUDE&lng=$LONGITUDE&date=today&formatted=0" | jq '.results.sunrise')
-
+SUNRISE=$(curl -s "https://api.sunrise-sunset.org/json?lat=$LATITUDE&lng=$LONGITUDE&date=$(date --date='next day' +'%Y-%m-%d')&formatted=0" | jq '.results.sunrise')
 SUNRISE="${SUNRISE%\"}"
 SUNRISE="${SUNRISE#\"}"
 
-echo "$(echo "$(date -d $SUNRISE +"%M")") $(echo "$(date -d $SUNRISE +"%H")") * * * /usr/bin/wakeonlan -i $BROADCAST_ADDRESS $MAC_ADDRESS && /usr/local/bin/sunrise-startup" > /etc/crontabs/root
+echo "$(echo "$(date -d $SUNRISE +"%M")") $(echo "$(date -d $SUNRISE +"%H")") $(date -d $SUNRISE +"%d") $(date -d $SUNRISE +"%m") * /usr/bin/wakeonlan -i $BROADCAST_ADDRESS $MAC_ADDRESS && /usr/local/bin/sunrise-startup" > /etc/crontabs/root
 
 /etc/init.d/cron restart
